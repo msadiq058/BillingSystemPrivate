a8e7e32ec00a07d3f9d363820b062a771dabcd9e
msadiq058 <42430865+msadiq058@users.noreply.github.com>
2021-02-09 19:02:36 +0530

    Update settings.py

--- BillingSystem/BillingSystem/settings.py
+++ BillingSystem/BillingSystem/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^ist0sbfk!p!rv1=n)yd24m6j@bj_4pyp9w)gv8=904p53+ib&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
