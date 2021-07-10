32b797eaf4107c536835c81b5f17db41ba60b788
Mohd Sadiq <itn.mohd.sadiq@grabtaxi.com>
2021-07-10 16:46:30 +0530

    Adding some files

--- BillingSystem/BillingSystem/settings.py
+++ BillingSystem/BillingSystem/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SECRET_KEY="wnsy(+l&5h7or__@acc&y@^)70b8ax27m#v5r*k%q&xeg6bfz*"
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
