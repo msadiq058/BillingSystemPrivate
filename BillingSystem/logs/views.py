999ee007e36c2a142693b22391e1225f9f60bc8a
Mohd Sadiq <itn.mohd.sadiq@grabtaxi.com>
2021-07-11 17:13:21 +0530

    Adding functionlity for home page

--- BillingSystem/logs/views.py
+++ BillingSystem/logs/views.py
@@ -6,6 +6,7 @@ from django import forms
 from django.forms import ModelChoiceField
 from .forms import NameForm
 from .utils import generateExcel
+from django.contrib import messages
 
 def print_details(request):
     name =''
@@ -18,10 +19,17 @@ def print_details(request):
             end_date = form.cleaned_data['end_date']
             print(name,start_date,end_date)
             data = Entry.objects.filter(firm_name=name,date__range=[start_date,end_date])
-            if(data.count()==0): print("No data found")
-            else: generateExcel(data,name,start_date,end_date)
+            if(data.count()==0): 
+                print("No data found")
+                messages.info(request, "No data found!")
+            else: 
+                generateExcel(data,name,start_date,end_date)
+
             # for i in data:
             #     print(str(i.date)+" "+str(i.firm_name)+" "+str(i.quantity)+" "+str(i.weight)+" "+str(i.remarks))
     else:
         form = NameForm
     return render(request,'logs/print_details.html',{'form':form})
+
+def home_page(request):
+    return render(request, 'logs/home_page.html')
